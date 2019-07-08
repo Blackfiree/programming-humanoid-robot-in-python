6dab280faad3a3062d3ae074a36862a6fa51c1df
Blackfiree <35333275+Blackfiree@users.noreply.github.com>
2019-07-08 23:20:42 +0200

    Add files via upload

--- distributed_computing/agent_server.py
+++ distributed_computing/agent_server.py
@@ -14,6 +14,8 @@
 # add PYTHONPATH
 import os
 import sys
+
+from SimpleXMLRPCServer import SimpleXMLRPCServer
 sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'kinematics'))
 
 from inverse_kinematics import InverseKinematicsAgent
@@ -27,33 +29,49 @@ class ServerAgent(InverseKinematicsAgent):
     def get_angle(self, joint_name):
         '''get sensor value of given joint'''
         # YOUR CODE HERE
+        return self.perception.joint[joint_name]
     
     def set_angle(self, joint_name, angle):
         '''set target angle of joint for PID controller
         '''
         # YOUR CODE HERE
+        self.perception.joint[joint_name] = angle
+        return 200
 
     def get_posture(self):
         '''return current posture of robot'''
         # YOUR CODE HERE
+        return self.recognize_posture(self.perception)
 
     def execute_keyframes(self, keyframes):
         '''excute keyframes, note this function is blocking call,
         e.g. return until keyframes are executed
         '''
         # YOUR CODE HERE
+        self.keyframes = keyframes
+        return 200
 
     def get_transform(self, name):
         '''get transform with given name
         '''
         # YOUR CODE HERE
+        return self.local_trans(name,self.perception.joint[name])
 
     def set_transform(self, effector_name, transform):
         '''solve the inverse kinematics and control joints use the results
         '''
         # YOUR CODE HERE
+        self.set_transform(effector_name,transform)
+        return 200
 
 if __name__ == '__main__':
     agent = ServerAgent()
+    
+    server = SimpleXMLRPCServer(("localhost", 8000))
+    server.register_introspection_functions()
+    server.register_instance(agent)
+    print ("Server on port 8000")
+    
+    server.serve_forever()
     agent.run()
 
